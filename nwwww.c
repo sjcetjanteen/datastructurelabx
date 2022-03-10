01d3b47ffd59a8416f5faf7a7d54fa146332060d
sjcetjanteen <101170582+sjcetjanteen@users.noreply.github.com>
2022-03-10 12:48:53 +0530

    Add files via upload

---
+++ nwwww.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int set1,set2,set3,seti,i,j,l1,l2;
+
+printf("enter the limit of the first set");
+scanf("%d",&l1);
+
+printf("enter the limit of the second set");
+scanf("%d",&l2);
+
+printf("enter the elements of the first set");
+
+for(i=1;i<l1;i++)
+{
+
+scanf("%d",&set1);
+
+}
+printf("enter the elements of the second set");
+for(i=1;i<l2;i++)
+{
+printf("elements",&set2);
+scanf("%d",&set2);
+
+}
+
+getch();
+}
